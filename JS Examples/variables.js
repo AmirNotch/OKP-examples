9489ea732ed992cba3821691cbfeb2026860a838
aibragimov <Ibragimov-amir@list.ru>
2024-07-24 11:26:43 +0500

    third

---
+++ JS Examples/variables.js
@@ -0,0 +1,24 @@
+var first = 5;
+var first = 10;
+
+var result = (first + (first * 3)) / 3;
+
+console.log(result); // 13.3333333
+
+let sum1 = 2;
+let sum2 = 3;
+
+sum1 = 10;
+
+let sumResult = sum1 + sum2 * first / first; // 32 | 10 | 5
+                                             // 13
+console.log(sumResult);
+
+const name = "Ivan";
+const surname = "Chishikov";
+
+const human = name + " " + surname;
+
+console.log(human);
+
+
